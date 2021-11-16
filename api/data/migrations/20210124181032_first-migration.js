945726be465abf284ad4f739936de8db56358e02
joseguevara29 <guevarabus29@gmail.com>
2021-11-15 20:10:32 -0500

    Finished db setup

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,50 @@
 exports.up = async (knex) => {
-  await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
-    })
-}
+  await knex.schema.createTable("users", (users) => {
+    users.increments("user_id");
+    users.string("username", 200).notNullable();
+    users.string("password", 200).notNullable();
+    users.string("role", 200).notNullable();
+  });
+  await knex.schema.createTable("events", (events) => {
+    events.increments("event_id");
+    events.string("event_name", 200).notNullable();
+    events.date("event_date").notNullable();
+    events.time("event_time").notNullable();
+    events.string("event_location", 200).notNullable();
+  });
+  await knex.schema.createTable("items", (items) => {
+    items.increments("item_id");
+    items.string("item_name", 200).notNullable();
+  });
+  await knex.schema.createTable("eu_appoint", (eu_appoint) => {
+    eu_appoint.increments("eu_id");
+    eu_appoint
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("user_id")
+      .inTable("users")
+      .onDelete("CASCADE");
+    eu_appoint
+      .integer("event_id")
+      .unsigned()
+      .notNullable()
+      .references("event_id")
+      .inTable("events")
+      .onDelete("CASCADE");
+    eu_appoint
+      .integer("item_id")
+      .unsigned()
+      .notNullable()
+      .references("item_id")
+      .inTable("items")
+      .onDelete("CASCADE");
+  });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("eu_appoint");
+  await knex.schema.dropTableIfExists("items");
+  await knex.schema.dropTableIfExists("events");
+  await knex.schema.dropTableIfExists("users");
+};
